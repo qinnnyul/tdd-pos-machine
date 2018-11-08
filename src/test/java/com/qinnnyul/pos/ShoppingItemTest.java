77bdad629a1b4893f6f1fa007190b655f9484900
Yulin Qin <yulin.qin.ext@gmail.com>
2018-11-09 07:56:03 +0800

    for normal case without discount, we calculate item with price + amount

---
+++ src/test/java/com/qinnnyul/pos/ShoppingItemTest.java
@@ -0,0 +1,18 @@
+package com.qinnnyul.pos;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+public class ShoppingItemTest {
+    @Test
+    public void shouldGetCostForSingleShoppingItem() throws Exception {
+        String name = "ITEM000001";
+        double price = 40d;
+        int amount = 2;
+        ShoppingItem shoppingItem = new ShoppingItem(name, price, amount);
+        double cost = shoppingItem.cost();
+        assertThat(cost, is(80d));
+    }
+}
